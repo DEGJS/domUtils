b9fd56c028ed17dfecc600cdb93b32da0324c013
Aaron Ladage <aladage@degdigital.com>
2017-10-07 16:22:19 -0500

    added replaceContent method

--- src/domUtils.js
+++ src/domUtils.js
@@ -1,60 +1,60 @@
-import { ensureArray } from "DEGJS/objectUtils";
+import {ensureArray} from "DEGJS/objectUtils";
 
 function isElement(o) {
     return (
         typeof HTMLElement === 'object' ? o instanceof HTMLElement : o && typeof o === 'object' && o !== null && o.nodeType === 1 && typeof o.nodeName==='string'
     ); 
-};
+}
 
 function createElement(nodeName, classNames) {
     classNames = ensureArray(classNames);
-    let el = document.createElement(nodeName);
-    classNames.forEach(function(className) {
-        el.classList.add(className);
-    });
+    const el = document.createElement(nodeName);
+    classNames.forEach(className => el.classList.add(className));
     return el;
-};
+}
 
 function emptyElements(els) {
     els = ensureArray(els);
-    els.forEach(function(el) {
+    els.forEach(el => {
         while (el.firstChild) {
             el.removeChild(el.firstChild);
         }
     });
-};
+}
+
+function replaceContent(el, newContent, position = 'afterbegin') {
+    emptyElements(el);
+    el.insertAdjacentHTML(position, newContent);
+}
 
 function removeElements(els) {
     els = ensureArray(els);
-    els.forEach(function(el) {
-        el.parentNode.removeChild(el);
-    });
-};
+    els.forEach(el => el.parentNode.removeChild(el));
+}
 
 function wrapElements(elsToWrap, wrapperEl) {
     elsToWrap = ensureArray(elsToWrap);
-    let firstElToWrap =  elsToWrap[0];
+    const firstElToWrap =  elsToWrap[0];
     firstElToWrap.parentNode.insertBefore(wrapperEl, firstElToWrap);
-    elsToWrap.forEach(function(elToWrap) {
-        wrapperEl.appendChild(elToWrap);
-    });
-};
+    elsToWrap.forEach(elToWrap => wrapperEl.appendChild(elToWrap));
+}
 
 function unwrapElements(wrapperEls) {
     wrapperEls = ensureArray(wrapperEls);
-    wrapperEls.forEach(function(wrapperEl) {
-        let fragment = document.createDocumentFragment();
+    wrapperEls.forEach(wrapperEl => {
+        const fragment = document.createDocumentFragment();
         while(wrapperEl.firstChild) {
             fragment.appendChild(wrapperEl.firstChild);
         }
         wrapperEl.parentNode.replaceChild(fragment, wrapperEl);
     });
-};
+}
 
 export { 
     isElement, 
     createElement, 
-    emptyElements, 
+    emptyElements,  
+    replaceContent,
     removeElements, 
     wrapElements, 
     unwrapElements 
